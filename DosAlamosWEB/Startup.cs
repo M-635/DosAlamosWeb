25f70cbde21e27786410e28e7cd9235f620023d6
Matías <M_635@hotmail.com>
2021-06-17 13:43:42 -0400

    Base MVC C#

---
+++ DosAlamosWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DosAlamosWEB.Startup))]
+namespace DosAlamosWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
